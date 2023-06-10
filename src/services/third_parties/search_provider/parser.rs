53acfd554adfc97df63b1d6dfddab98a288febe5
Roman L <roman.lakhtadyr@gmail.com>
2023-06-10 23:24:03 +0100

    Sort parse_search_results

--- src/services/third_parties/search_provider/parser.rs
+++ src/services/third_parties/search_provider/parser.rs
@@ -16,6 +16,31 @@ pub(crate) struct SearchResult {
     pub(crate) seeds_number: u64,
 }
 
+fn get_search_result_priority(result: &SearchResult) -> usize {
+    match result {
+        res if res.title.contains("tracks")
+            && res.title.contains("FLAC (tracks)")
+            && res.seeds_number > 1 =>
+        {
+            1
+        }
+        res if res.title.contains("tracks")
+            && res.title.contains("MP3, 320 kbps")
+            && res.seeds_number > 1 =>
+        {
+            2
+        }
+        res if res.title.contains("tracks")
+            && res.title.contains("AAC")
+            && res.seeds_number > 1 =>
+        {
+            3
+        }
+        res if res.seeds_number > 1 => 4,
+        _ => 10,
+    }
+}
+
 pub(crate) fn parse_search_results(raw: &str) -> Result<Vec<SearchResult>, ParseError> {
     let html = Html::parse_document(raw);
 
@@ -27,7 +52,7 @@ pub(crate) fn parse_search_results(raw: &str) -> Result<Vec<SearchResult>, Parse
     let td_selector = &Selector::parse(r#"td"#).unwrap();
     let seeds_selector = &Selector::parse(r#"b.seedmed"#).unwrap();
 
-    Ok(table_entries
+    let mut results: Vec<_> = table_entries
         .skip(1)
         .filter(|el| el.children().filter(|el| el.value().is_element()).count() == 10)
         .map(|el| {
@@ -53,7 +78,12 @@ pub(crate) fn parse_search_results(raw: &str) -> Result<Vec<SearchResult>, Parse
                 seeds_number,
             }
         })
-        .collect())
+        .filter(|r| !r.title.contains("image+.cue"))
+        .collect();
+
+    results.sort_by(|a, b| get_search_result_priority(a).cmp(&get_search_result_priority(b)));
+
+    Ok(results)
 }
 
 #[cfg(test)]
@@ -66,24 +96,16 @@ mod tests {
             .expect("Expected successful parse results");
 
         let expected_results = vec![
-            SearchResult { title: "(Dream-House/Dance/Trance) [LP] [24/96] Robert Miles - Dreamland - 2016 (1996), FLAC (image+.cue)".into(), topic_id: 6213241.into(), seeds_number: 6 },
-            SearchResult { title: "(Dream-House/Dance/Trance) [2xLP's] [24/192] Robert Miles - Dreamland (Exclusive in Russia) © 2019 (1996), WavPack (image+.cue)".into(), topic_id: 5744476.into(), seeds_number: 6 }, 
+            SearchResult { title: "(Trance) [WEB] Robert Miles - Dreamland (Remastered) - 2016, FLAC (tracks), lossless".into(), topic_id: 5318721.into(), seeds_number: 8 },
+            SearchResult { title: "(Trance) Robert Miles - Dreamland (Remastered) - 2016, MP3, 320 kbps".into(), topic_id: 5309922.into(), seeds_number: 9 },
             SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland - 1996 (Deconstruction [74321 42974 2]), FLAC (tracks+.cue), lossless".into(), topic_id: 1183770.into(), seeds_number: 18 }, 
             SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland (The Winter Edition) - 1996 (Urban #533 791-2), FLAC (tracks+.cue), lossless".into(), topic_id: 1184081.into(), seeds_number: 11 }, 
-            SearchResult { title: "(Trance) [WEB] Robert Miles - Dreamland (Remastered) - 2016, FLAC (tracks), lossless".into(), topic_id: 5318721.into(), seeds_number: 8 }, 
-            SearchResult { title: "(Trance) Robert Miles - Dreamland (Remastered) - 2016, MP3, 320 kbps".into(), topic_id: 5309922.into(), seeds_number: 9 }, 
-            SearchResult { title: "[DTSCD][UP] Robert Miles - Dreamland - 1996 (Trance, Electronic, Ambient, House)".into(), topic_id: 5217604.into(), seeds_number: 5 }, 
             SearchResult { title: "(Dream House) Robert Miles - Dreamland (Including One and One) - 1996 [WEB], AAC (tracks) 256 kbps".into(), topic_id: 4737164.into(), seeds_number: 2 }, 
-            SearchResult { title: "[DTSCD][UP] Robert Miles - Dreamland - 1996, (Electronica, alternative, dance, ambient)".into(), topic_id: 3901716.into(), seeds_number: 4 }, 
             SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland - 1996, FLAC (tracks+.cue) lossless".into(), topic_id: 3418878.into(), seeds_number: 4 }, 
-            SearchResult { title: "(Trance) Robert Miles - Dreamland - 1996 (Urban, 533 002-2), FLAC (image+.cue) lossless".into(), topic_id: 3199643.into(), seeds_number: 10 }, 
-            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland~New Edition (Japan) - 1996, FLAC (image+.cue), lossless".into(), topic_id: 2495343.into(), seeds_number: 7 }, 
-            SearchResult { title: "(Фортепиано, Голос, Аккорды) Robert Miles - Dreamland (Children, Fable и др.) [1997, PDF, ENG]".into(), topic_id: 1295160.into(), seeds_number: 2 }, 
-            SearchResult { title: "Robert Miles - Dreamland - 1996, ALAC, lossless".into(), topic_id: 1201152.into(), seeds_number:3 }, 
-            SearchResult { title: "(Trance, Dreamhouse) Robert Miles - Dreamland - 1996, APE (image+.cue), lossless".into(), topic_id: 1182981.into(), seeds_number: 2 }
+            SearchResult { title: "Robert Miles - Dreamland - 1996, ALAC, lossless".into(), topic_id: 1201152.into(), seeds_number: 3 }, 
         ];
 
-        assert_eq!(15, results.len());
+        assert_eq!(10, results.len());
         assert_eq!(expected_results, results);
     }
 }
