d638498f65bfe0815789a3107b551639b4fd6c84
Roman L <roman.lakhtadyr@gmail.com>
2023-06-11 15:55:20 +0100

    Improve results sorting order

--- src/services/third_parties/search_provider/parser.rs
+++ src/services/third_parties/search_provider/parser.rs
@@ -1,12 +1,11 @@
 use crate::types::TopicId;
+use scraper::error::SelectorErrorKind;
 use scraper::{Html, Selector};
 
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum ParseError {
-    #[error("Unable to select table with search results")]
-    TableSelectorError,
-    #[error("Unexpected")]
-    Unexpected,
+    #[error(transparent)]
+    SelectorError(#[from] SelectorErrorKind<'static>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -16,41 +15,67 @@ pub(crate) struct SearchResult {
     pub(crate) seeds_number: u64,
 }
 
-fn get_search_result_priority(result: &SearchResult) -> usize {
-    match result {
-        res if res.title.contains("tracks")
-            && res.title.contains("FLAC (tracks)")
-            && res.seeds_number > 1 =>
-        {
-            1
-        }
-        res if res.title.contains("tracks")
-            && res.title.contains("MP3, 320 kbps")
-            && res.seeds_number > 1 =>
-        {
-            2
-        }
-        res if res.title.contains("tracks")
-            && res.title.contains("AAC")
-            && res.seeds_number > 1 =>
-        {
-            3
-        }
-        res if res.seeds_number > 1 => 4,
-        _ => 10,
+impl SearchResult {
+    pub(crate) fn bitrate_hint(&self) -> Option<u64> {
+        let words = self.title.split(' ').collect::<Vec<_>>();
+        let bitrate_str = words.iter().zip(words.iter().skip(1)).find_map(|(a, b)| {
+            if b == &"kbps" {
+                Some(a.to_string())
+            } else {
+                None
+            }
+        });
+
+        bitrate_str.and_then(|s| s.parse::<u64>().ok())
     }
+}
+
+const AUDIO_FORMAT_PRIORITY: [&str; 4] = ["FLAC", "MP3", "ALAC", "AAC"];
+const AUDIO_BITRATE_PRIORITY: [&str; 3] = ["lossless", "320 kbps", "256 kbps"];
+
+fn get_search_result_priority(result: &SearchResult) -> usize {
+    let format_priority = AUDIO_FORMAT_PRIORITY
+        .iter()
+        .enumerate()
+        .find_map(|(i, format)| {
+            if result.title.contains(format) {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .unwrap_or(10);
+    let bitrate_priority = AUDIO_BITRATE_PRIORITY
+        .iter()
+        .enumerate()
+        .find_map(|(i, bitrate)| {
+            if result.title.contains(bitrate) {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .unwrap_or(10);
+    let seeds_priority = match result.seeds_number {
+        x if x == 0 => 10,
+        x if x < 10 => 3,
+        x if x < 20 => 2,
+        x if x < 30 => 1,
+        _ => 0,
+    };
+
+    format_priority * 5 + bitrate_priority * 10 + seeds_priority
 }
 
 pub(crate) fn parse_search_results(raw: &str) -> Result<Vec<SearchResult>, ParseError> {
     let html = Html::parse_document(raw);
 
-    let table_row_selector =
-        Selector::parse(r#"table.forumline tr"#).map_err(|_| ParseError::TableSelectorError)?;
+    let table_row_selector = Selector::parse(r#"table.forumline tr"#)?;
     let table_entries = html.select(&table_row_selector);
 
-    let href_selector = &Selector::parse(r#"a[href]"#).unwrap();
-    let td_selector = &Selector::parse(r#"td"#).unwrap();
-    let seeds_selector = &Selector::parse(r#"b.seedmed"#).unwrap();
+    let href_selector = Selector::parse(r#"a[href]"#)?;
+    let td_selector = Selector::parse(r#"td"#)?;
+    let seeds_selector = Selector::parse(r#"b.seedmed"#)?;
 
     let mut results: Vec<_> = table_entries
         .skip(1)
@@ -58,18 +83,31 @@ pub(crate) fn parse_search_results(raw: &str) -> Result<Vec<SearchResult>, Parse
         .filter_map(|el| {
             let columns = el.select(&td_selector).collect::<Vec<_>>();
             let link = columns[3].select(&href_selector).next()?;
+            let category_str = columns[2]
+                .select(&href_selector)
+                .next()?
+                .inner_html()
+                .to_lowercase();
+
+            if !category_str.contains("loss") {
+                return None;
+            }
 
             let title = link.inner_html().to_string();
-
-            let topic_id_raw = link.value().attr("href")?.to_string();
-            let topic_id = topic_id_raw
-                .replace("viewtopic.php?t=", "")
+            let topic_id = link
+                .value()
+                .attr("data-topic_id")?
+                .to_string()
                 .parse::<u64>()
                 .ok()?
                 .into();
-
-            let seeds = columns[6].select(&seeds_selector).next()?;
-            let seeds_number = seeds.inner_html().to_string().parse().ok()?;
+            let seeds_number = columns[6]
+                .select(&seeds_selector)
+                .next()?
+                .inner_html()
+                .to_string()
+                .parse()
+                .ok()?;
 
             Some(SearchResult {
                 title,
@@ -95,16 +133,16 @@ mod tests {
             .expect("Expected successful parse results");
 
         let expected_results = vec![
+            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland - 1996 (Deconstruction [74321 42974 2]), FLAC (tracks+.cue), lossless".into(), topic_id: 1183770.into(), seeds_number: 18 },
+            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland (The Winter Edition) - 1996 (Urban #533 791-2), FLAC (tracks+.cue), lossless".into(), topic_id: 1184081.into(), seeds_number: 11 },
             SearchResult { title: "(Trance) [WEB] Robert Miles - Dreamland (Remastered) - 2016, FLAC (tracks), lossless".into(), topic_id: 5318721.into(), seeds_number: 8 },
+            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland - 1996, FLAC (tracks+.cue) lossless".into(), topic_id: 3418878.into(), seeds_number: 4 },
+            SearchResult { title: "Robert Miles - Dreamland - 1996, ALAC, lossless".into(), topic_id: 1201152.into(), seeds_number: 3 },
             SearchResult { title: "(Trance) Robert Miles - Dreamland (Remastered) - 2016, MP3, 320 kbps".into(), topic_id: 5309922.into(), seeds_number: 9 },
-            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland - 1996 (Deconstruction [74321 42974 2]), FLAC (tracks+.cue), lossless".into(), topic_id: 1183770.into(), seeds_number: 18 }, 
-            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland (The Winter Edition) - 1996 (Urban #533 791-2), FLAC (tracks+.cue), lossless".into(), topic_id: 1184081.into(), seeds_number: 11 }, 
-            SearchResult { title: "(Dream House) Robert Miles - Dreamland (Including One and One) - 1996 [WEB], AAC (tracks) 256 kbps".into(), topic_id: 4737164.into(), seeds_number: 2 }, 
-            SearchResult { title: "(Trance, Dream House, Downtempo) Robert Miles - Dreamland - 1996, FLAC (tracks+.cue) lossless".into(), topic_id: 3418878.into(), seeds_number: 4 }, 
-            SearchResult { title: "Robert Miles - Dreamland - 1996, ALAC, lossless".into(), topic_id: 1201152.into(), seeds_number: 3 }, 
+            SearchResult { title: "(Dream House) Robert Miles - Dreamland (Including One and One) - 1996 [WEB], AAC (tracks) 256 kbps".into(), topic_id: 4737164.into(), seeds_number: 2 },
         ];
 
-        assert_eq!(10, results.len());
+        assert_eq!(7, results.len());
         assert_eq!(expected_results, results);
     }
 }
